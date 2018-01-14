ea70323dd43baff0915c4027698f24e032a71741
unknown <nray24@gmail.com>
2018-01-14 14:50:39 -0700

    14.50

---
+++ assets/javascript/rpsm.js
@@ -0,0 +1,11 @@
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDx0AWYiagYCV-KrEwLWdAqHouA8DEpRqA",
+  authDomain: "rpsm-176e3.firebaseapp.com",
+  databaseURL: "https://rpsm-176e3.firebaseio.com",
+  projectId: "rpsm-176e3",
+  storageBucket: "rpsm-176e3.appspot.com",
+  messagingSenderId: "1085251346690"
+};
+firebase.initializeApp(config);
